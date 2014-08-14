c6d7c5c25191198f5aa15cb39ee70cff6542e6d8
Lex Alexander <l.alexander10@gmail.com>
2014-08-13 21:51:41 -0700

    I hope this works.

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 var app = angular.module("app",[]);
+
+app.controller("table_controller", function($scope)
+	$scope.sorted_columns = []; 
+	$scope.all_fields = [{
+		"title": "name", 
+		"type": "string"
+		"checked": true 
+	},
+	{
+		"title": "age",
+		"type": "number", 
+		"checked": true
+
+	},
+	{
+		"title": "occupation", 
+		"type": "string",
+		"checked": true 
+
+	}		
+
+
+	}];
+
+	$scope.data = [{
+		"name": "Bill Gates",
+		"age": "58",
+		"occupation": "Richer than you"
+	},
+	{	"name": "Barack Obama", 
+
+
+
+
+	}];
+
+	$scope.$watch("all_fields")	
